4b3fb7bd59eb22a429fb99cf40e65e7654c77c73
Chaowenwen <2411055299@qq.com>
2024-03-13 08:58:23 +0800

    0311first

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -23,16 +23,9 @@ public class Grades {
     * The method to reverse the internal Java int array.
     */
    public void reverse() {
-      /* TODO:
-       1. Edit the test data files to see if the reverse() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
-      */
+     
       int i = 0;
-      while (i <= grades.length/2) {
+      while(i < grades.length/2) {
          int temp = grades[i];
          grades[i] = grades[grades.length-i-1];
          grades[grades.length-i-1] = temp;
@@ -44,24 +37,21 @@ public class Grades {
     * Sorts the array to ascending order.
     */
    public void sort() {
-      /* TODO:
-       1. Edit the test data files to see if the sort() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
-      */
-      int i = grades.length-1;
-      while (i > 0) {
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
-         }
-         i--;
-      }
-   }
+      
+      boolean swapped;  
+      do {  
+          swapped = false;  
+          for (int i = 0; i < grades.length-1; i++) {  
+              if (grades[i] > grades[i+1]) {  
+                  int temp = grades[i];  
+                  grades[i] = grades[i+1];  
+                  grades[i+1] = temp;  
+                  swapped = true;  
+              }  
+          }  
+      } while (swapped);  
+  }
+  
 
    /**
     * Returns the plain Java int [] array for investigation.
