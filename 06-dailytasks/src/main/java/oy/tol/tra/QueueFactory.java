c5d0b4c020aaeb5ffbe7b0de1c6532d91843d663
Chaowenwen <2411055299@qq.com>
2024-04-03 22:07:43 +0800

    hhh

---
+++ 06-dailytasks/src/main/java/oy/tol/tra/QueueFactory.java
@@ -0,0 +1,35 @@
+package oy.tol.tra;
+
+/**
+ * This class instantiates a queue implementing the {@code QueueInterface}.
+ * 
+ * @author Antti Juustila
+ */
+public class QueueFactory {
+
+   private QueueFactory() {
+   }
+
+   /**
+    * Creates an instance of QueueInterface for Integer type.
+    * @param capacity Number of elements the queue can hold.
+    * @return The queue object.
+    */
+   public static QueueInterface<Integer> createIntegerQueue(int capacity) {
+      return new QueueImplementation<>(capacity);
+   }
+
+   /**
+    * Creates an instance of QueueInterface for Integer type.
+    * Use the default constructor of the queue.
+    * @return The queue object.
+    */
+   public static QueueInterface<Integer> createIntegerQueue() {
+      int defaultCapacity = 10;
+      return new QueueImplementation<>(defaultCapacity);
+   }
+
+   public static QueueInterface<String> createStringQueue() {
+      return new QueueImplementation<String>();
+   }
+}
